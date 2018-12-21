7663a2b884da8cb7be0247aef34b075948d6b6ae
Mekhi Jones <mekhi.rafiki@gmail.com>
2018-12-21 18:08:01 -0500

    MVP For Tasks Addition
    Added in a text field and javascript to pair with it to add tasks to an unordered list. Future work will include utilizing the localStorage then a database for this content.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+
+
+
+function load() {
+  const taskSubmitBtn = document.getElementById("taskSubmit");
+  const tasksList = document.getElementById("taskList");
+
+  taskSubmitBtn.addEventListener("click", event =>{
+    event.preventDefault();
+    if(event.target.type === "submit"){
+      	const tasksInput = document.getElementById("newTaskText");
+        const taskText = tasksInput.value;
+        tasksInput.innerHTML = "";
+        console.log(taskText);
+
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(taskText);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        tasksList.appendChild(node);
+    }
+  });
+
+}
+
+
+window.onload = load;
