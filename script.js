285127bea8af77f0464039071b6fe4b5ae90a474
Mekhi Jones <mekhi.rafiki@gmail.com>
2018-12-21 18:53:31 -0500

    MVP for Roles to Goals Pane
    Simply allow the static addition of Roles and the dynamic addition of goals for each role.

--- script.js
+++ script.js
@@ -3,6 +3,23 @@
 
 
 function load() {
+
+  /* Roles -> Goals Area */
+  const rolesPane = document.getElementById("roles");
+  rolesPane.addEventListener("click", event=>{
+    event.preventDefault();
+    if(event.target.type === "submit"){
+      const goalList = event.path[2].getElementsByTagName('ul')[0];
+      const newGoal = event.path[2].getElementsByTagName('textarea')[0].value;
+
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(newGoal);
+      node.appendChild(textnode);
+      goalList.appendChild(node);
+      event.path[2].getElementsByTagName('textarea')[0].value ='';
+    }
+  });
+  /* Task Submission Area */
   const taskSubmitBtn = document.getElementById("taskSubmit");
   const tasksList = document.getElementById("taskList");
 
@@ -14,9 +31,9 @@ function load() {
         tasksInput.innerHTML = "";
         console.log(taskText);
 
-        var node = document.createElement("LI");                 // Create a <li> node
-        var textnode = document.createTextNode(taskText);         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(taskText);
+        node.appendChild(textnode);
         tasksList.appendChild(node);
     }
   });
