9cb2a1788a861f82d2a09f322ba90a218dfe4f40
Mekhi Jones <mekhi.rafiki@gmail.com>
2019-01-04 22:15:26 -0500

    Add in Drag-Drop Functionality for Daily Tasks
    Add in drag-drop functionality for weekly tasks. Took out the manual priority listing

--- script.js
+++ script.js
@@ -137,21 +137,15 @@ function loadTaskSubmit(){
     event.preventDefault();
     if(event.target.type === "submit"){
         const tasksInput = document.getElementById("newTaskText");
-        const taskPriority = document.getElementById("newTaskPriority");
         /* Add to Local Storage */
         var newTask = {
-          task: tasksInput.value,
-          priority: taskPriority.value
+          task: tasksInput.value
         }
         currTasks.push(newTask);
-        // Sort by priority
-        currTasks.sort(function(a,b){
-          return a.priority - b.priority;
-        });
+
         localStorage.T = JSON.stringify(currTasks);
 
         tasksInput.value = ""; // Clear the screen
-        taskPriority.value ="";
 
         populateCurrTasks(currTasks);
     }
@@ -226,7 +220,7 @@ function createRGElement(element){
   ])
 }
 function createTaskElement(task){
-  return tag("div", {class: "specificTask"}, [
+  return tag("li", {class: "specificTask", draggable:"true"}, [
     tag("h4", {class: "taskHeader"}, task),
     tag("img", {class: "statusBtn", src: "assets/CheckMark.png", alt:"Complete", height: "15", width: "15", id: "TaskComplete"}, []),
     tag("img", {class: "statusBtn", src: "assets/X-out.png", alt:"Cancel", height: "15", width: "15"}, [])
@@ -259,13 +253,90 @@ function load() {
   loadRoleGoals();
   loadWeeklyPriority();
   loadTaskSubmit();
+
+  var cols = document.querySelectorAll('.specificTask');
+  [].forEach.call(cols, addDnDHandlers);
 }
 
 window.onload = load;
 
 /*
+  Draggable Code
+*/
+var dragSrcEl = null;
 
+function handleDragStart(e) {
+  // Target (this) element is the source node.
+  dragSrcEl = this;
 
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.outerHTML);
+
+  this.classList.add('dragElem');
+}
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault(); // Necessary. Allows us to drop.
+  }
+  this.classList.add('over');
+
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  // this / e.target is the current hover target.
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');  // this / e.target is previous target element.
+}
+
+function handleDrop(e) {
+  // this/e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    //alert(this.outerHTML);
+    //dragSrcEl.innerHTML = this.innerHTML;
+    //this.innerHTML = e.dataTransfer.getData('text/html');
+    this.parentNode.removeChild(dragSrcEl);
+    var dropHTML = e.dataTransfer.getData('text/html');
+    this.insertAdjacentHTML('beforebegin',dropHTML);
+    var dropElem = this.previousSibling;
+    addDnDHandlers(dropElem);
+
+  }
+  this.classList.remove('over');
+  return false;
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+  this.classList.remove('over');
+
+  /*[].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });*/
+}
+
+function addDnDHandlers(elem) {
+  elem.addEventListener('dragstart', handleDragStart, false);
+  elem.addEventListener('dragenter', handleDragEnter, false)
+  elem.addEventListener('dragover', handleDragOver, false);
+  elem.addEventListener('dragleave', handleDragLeave, false);
+  elem.addEventListener('drop', handleDrop, false);
+  elem.addEventListener('dragend', handleDragEnd, false);
+
+}
+
+/*
   Code from W3
 
 */
